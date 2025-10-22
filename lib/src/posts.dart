a95bb26c162a52de6beb3762219353c5db4b95dd
Caio Onha Ferreira <caioonha@gmail.com>
2025-10-22 19:57:53 -0300

    Consumindo API

--- lib/src/posts.dart
+++ lib/src/posts.dart
@@ -39,6 +39,41 @@ class PostsState extends State<Posts> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Posts"),
+        ),
+        body: loading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tasks[index]['title'],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(tasks[index]['body']),
+                    ],
+                  )
+                );
+              },
+            ),
+      ),
+    );
   }
 }
